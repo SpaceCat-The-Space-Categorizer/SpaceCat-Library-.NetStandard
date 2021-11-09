54a98b3d18d4f2c456bd984f1206cd9280642e91
Ryan Beck <rb200@humboldt.edu>
2021-11-09 00:55:33 -0800

    adding of newly refactored code to make it work with .Net Standard 2.0

---
+++ SurveyStructs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SpaceCat
+{
+    public struct AreaSurvey
+    {
+        public int AreaID { get; }
+        public DateTime TimeSurveyed { get; }
+        public int SurveyNumber { get; }
+        public int FilledSeats { get; }
+        public string AdditionalNotes { get; }
+
+        public AreaSurvey(int areaID, int surveyNumber, int filledSeats, string additionalNotes) : this()
+        {
+            AreaID = areaID;
+            TimeSurveyed = DateTime.Now;
+            SurveyNumber = surveyNumber;
+            FilledSeats = filledSeats;
+            AdditionalNotes = additionalNotes;
+        }
+    }
+}
