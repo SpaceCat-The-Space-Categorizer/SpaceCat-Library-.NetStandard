f06cb3ac6f33f7d2eceb3075078063b7c07a47fa
Riley Heffernan <rah161@humboldt.edu>
2021-11-16 13:50:17 -0800

    Added recent buildings persistence file and fixed bugs from 0.1.2

---
+++ SCUL/Persistence.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace SpaceCat
+{
+    public static class Persistence
+    {
+        public static string BaseFilePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        private static string PERSISTENCE = "persistence.json";
+
+        public static string BuildPath(string fileName, string folderName = null)
+        {
+            if (folderName == null) return Path.Combine(BaseFilePath, fileName);
+            else return Path.Combine(BaseFilePath, folderName, fileName);
+        }
+        public static void SaveRecentBuildings(List<RecentBuilding> recentBuildings)
+        {
+            try
+            {
+                using (FileStream fs = File.OpenWrite(BuildPath(PERSISTENCE)))
+                {
+                    byte[] byteArray = JsonSerializer.SerializeToUtf8Bytes(recentBuildings);
+                    fs.Write(byteArray, 0, byteArray.Length);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                return;
+            }
+        }
+
+        public static List<RecentBuilding> LoadRecentBuildings()
+        {
+            try
+            {
+                using (FileStream fs = File.OpenRead(BuildPath(PERSISTENCE)))
+                {
+                    byte[] byteArray = new byte[2048];
+                    int bytesRead = fs.Read(byteArray, 0, byteArray.Length);
+                    string jsonString = Encoding.UTF8.GetString(byteArray);
+                    return JsonSerializer.Deserialize<List<RecentBuilding>>(jsonString);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return new List<RecentBuilding>();
+            }
+        }
+
+    }
+}
---
+++ SCUL/RecentBuilding.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceCat
+{
+    public struct RecentBuilding
+    {
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        public string FilePath { get; set; }
+        public DateTime TimeAccessed { get; set; }
+
+        public RecentBuilding(Building accessedBuilding)
+        {
+            Name = accessedBuilding.Name;
+            DateCreated = accessedBuilding.DateCreated;
+
+            FilePath =  Persistence.BuildPath(accessedBuilding.Name);
+            TimeAccessed = DateTime.Now;
+        }
+    }
+}
